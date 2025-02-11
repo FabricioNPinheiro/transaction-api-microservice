50eceee953c2f14ddd14653eb712ced04fbee696
Fabrício Nogueira <fabricionogueira@MacBook-Pro-de-Fabricio.local>
2025-02-11 20:28:40 -0300

    chore: initial structure and project setup.

---
+++ src/domain/gateway/transaction.gateway.ts
@@ -0,0 +1,51 @@
+export type TransactionProps = {
+    id: string;
+    senderUserId: string;
+    receiverUserId: string;
+    amount: number;
+    description: string;
+  };
+  
+  export class Transaction {
+    private constructor(private props: TransactionProps) {}
+  
+    public static create(
+      senderUserId: string,
+      receiverUserId: string,
+      amount: number,
+      description: string
+    ) {
+      return new Transaction({
+        id: crypto.randomUUID().toString(),
+        senderUserId,
+        receiverUserId,
+        amount,
+        description,
+      });
+    }
+  
+    public static with(props: TransactionProps) {
+      return new Transaction(props);
+    }
+  
+    public get id() {
+      return this.props.id;
+    }
+  
+    public get senderUserId() {
+      return this.props.senderUserId;
+    }
+  
+    public get receiverUserId() {
+      return this.props.receiverUserId;
+    }
+  
+    public get amount() {
+      return this.props.amount;
+    }
+  
+    public get description() {
+      return this.props.description;
+    }
+  }
+  
